9f33c2d8696fae941bedcb51721ef2179bc8d773
rahul4019 <rahulkashyap4019@gmail.com>
2023-01-27 22:33:08 +0530

    Add indexpage component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,12 @@
-import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import IndexPage from "./pages/IndexPage";
 
 function App() {
-  return <div className="text-3xl bg-slate-400">test</div>;
+  return (
+    <Routes>
+      <Route index element={<IndexPage />} />
+    </Routes>
+  );
 }
 
 export default App;
--- client/tailwind.config.cjs
+++ client/tailwind.config.cjs
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        primary: '#F5385D'
+      }
+    },
   },
   plugins: [],
 };
