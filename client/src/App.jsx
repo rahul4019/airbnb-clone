da63dc20673ea511977a28d9df8920a69f82ae5e
rahul4019 <rahulkashyap4019@gmail.com>
2023-02-14 17:53:24 +0530

    Change localhost url with hosted api url

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,7 +12,7 @@ import PlacePage from "./pages/PlacePage";
 import BookingsPage from "./pages/BookingsPage";
 import BookingPage from "./pages/BookingPage";
 
-axios.defaults.baseURL = "http://localhost:4000";
+axios.defaults.baseURL = "https://airbnb-clone-api.onrender.com";
 axios.defaults.withCredentials = true;
 
 function App() {
