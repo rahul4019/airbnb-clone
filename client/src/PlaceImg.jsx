68ef3ae028317cc222e89bc7668a5838bd2eb143
rahul4019 <rahulkashyap4019@gmail.com>
2023-02-17 17:45:56 +0530

    Change img url

--- client/src/PlaceImg.jsx
+++ client/src/PlaceImg.jsx
@@ -1,16 +1,16 @@
-import React from "react";
+import React from 'react';
 
-const PlaceImg = ({ place, index = 0, className=null }) => {
+const PlaceImg = ({ place, index = 0, className = null }) => {
   if (!place.photos?.length) {
-    return "";
+    return '';
   }
-  if(!className) {
-    className = 'object-cover'
+  if (!className) {
+    className = 'object-cover';
   }
   return (
     <div>
       <img
-        src={`http://localhost:4000/uploads/${place.photos[index]}`}
+        src={`https://airbnb-clone-api.onrender.com/uploads/${place.photos[index]}`}
         alt=""
         className={className}
       />
