7701d9bcba75f4e94f8168fc259442833f2f1905
rahul4019 <rahulkashyap4019@gmail.com>
2023-05-28 18:01:36 +0530

    Fix url error

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -20,7 +20,7 @@ const token = getItemFromLocalStorage('token');
 
 // axios.defaults.baseURL = 'https://airbnb-clone-production.up.railway.app';
 axios.defaults.baseURL = 'https://airbnb-api-5s7s.onrender.com';
-axios.defaults.baseURL = 'http://localhost:4000';
+// axios.defaults.baseURL = 'http://localhost:4000';
 axios.defaults.withCredentials = true;
 axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
 
