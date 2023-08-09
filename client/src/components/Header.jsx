cdf981a86f26c19a1f353fcb16833170ec8ecb37
rahul4019 <rahulkashyap4019@gmail.com>
2023-08-09 07:49:50 +0530

    Fix logo refresh and handle no place found UI

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -43,7 +43,7 @@ export const Header = () => {
           showSearchBar ? 'justify-around' : 'justify-between px-10'
         } w-screen max-w-screen-xl`}
       >
-        <Link to={'/'} className="flex items-center gap-1">
+        <a href="/" className="flex items-center gap-1">
           <img
             className="h-8 w-8 md:h-10 md:w-10"
             src="https://cdn-icons-png.flaticon.com/512/2111/2111320.png"
@@ -53,7 +53,7 @@ export const Header = () => {
           <span className="hidden md:block font-bold text-2xl text-red-500">
             airbnb
           </span>
-        </Link>
+        </a>
 
         {showSearchBar && <SearchBar />}
 
@@ -81,21 +81,20 @@ export const Header = () => {
             version="1.1"
             id="Layer_1"
             xmlns="http://www.w3.org/2000/svg"
-            xmlns:xlink="http://www.w3.org/1999/xlink"
+            xmlnsXlink="http://www.w3.org/1999/xlink"
             viewBox="796 796 200 200"
-            enable-background="new 796 796 200 200"
-            xml:space="preserve"
+            enableBackground="new 796 796 200 200"
+            xmlSpace="preserve"
             stroke="#858080"
             className="w-8 h-8"
           >
-            <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+            <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
             <g
               id="SVGRepo_tracerCarrier"
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeLinecap="round"
+              strokeLinejoin="round"
             ></g>
             <g id="SVGRepo_iconCarrier">
-              {' '}
               <path d="M896,796c-55.14,0-99.999,44.86-99.999,100c0,55.141,44.859,100,99.999,100c55.141,0,99.999-44.859,99.999-100 C995.999,840.86,951.141,796,896,796z M896.639,827.425c20.538,0,37.189,19.66,37.189,43.921c0,24.257-16.651,43.924-37.189,43.924 s-37.187-19.667-37.187-43.924C859.452,847.085,876.101,827.425,896.639,827.425z M896,983.86 c-24.692,0-47.038-10.239-63.016-26.695c-2.266-2.335-2.984-5.775-1.84-8.82c5.47-14.556,15.718-26.762,28.817-34.761 c2.828-1.728,6.449-1.393,8.91,0.828c7.706,6.958,17.316,11.114,27.767,11.114c10.249,0,19.69-4.001,27.318-10.719 c2.488-2.191,6.128-2.479,8.932-0.711c12.697,8.004,22.618,20.005,27.967,34.253c1.144,3.047,0.425,6.482-1.842,8.817 C943.037,973.621,920.691,983.86,896,983.86z"></path>{' '}
             </g>
           </svg>
--- client/src/pages/IndexPage.jsx
+++ client/src/pages/IndexPage.jsx
@@ -13,7 +13,7 @@ const IndexPage = () => {
 
   return (
     <div className="mt-24 px-10 grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4  gap-x-6 gap-y-8 ">
-      {places.length > 0 &&
+      {places.length > 0 ? (
         places.map((place) => (
           <Link to={`/place/${place._id}`} key={place._id}>
             <div className="bg-gray-500 mb-2 rounded-2xl flex">
@@ -26,7 +26,35 @@ const IndexPage = () => {
               per night
             </div>
           </Link>
-        ))}
+        ))
+      ) : (
+        <div className="flex flex-col w-full p-10 md:w-1/2  absolute left-1/2 right-1/2 transform -translate-x-1/2  top-40">
+          <h1 className="font-semibold text-3xl">Result not found!</h1>
+          <p className="font-semibold text-lg">
+            Sorry, we couldn&#39;t find the place you&#39;re looking for.
+          </p>
+          <button className="w-32 p-2 mt-4 rounded-full bg-primary text-white">
+            <a href="/" className="flex gap-1 items-center justify-center">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                className="w-5 h-5"
+              >
+                <line x1="19" y1="12" x2="5" y2="12"></line>
+                <polyline points="12 19 5 12 12 5"></polyline>
+              </svg>
+              Go back
+            </a>
+          </button>
+        </div>
+      )}
     </div>
   );
 };
