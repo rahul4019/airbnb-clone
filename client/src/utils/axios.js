4a0f5bf65a1bc478d692477f58d2b38d1daedd14
rahul4019 <rahulkashyap4019@gmail.com>
2023-08-17 17:51:47 +0530

    Host api on AWS

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: 'https://airbnb-api-5s7s.onrender.com',
+    // baseURL: 'https://airbnb-api-5s7s.onrender.com',
     // baseURL: 'http://localhost:4000',
+    baseURL: proccess.env.BASE_URL,
     withCredentials: true,
 })
 
