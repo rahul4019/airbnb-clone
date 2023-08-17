fc448762baf74438c9418f938a3b78c9aa3b5121
rahul4019 <rahulkashyap4019@gmail.com>
2023-08-17 18:06:57 +0530

    env variable

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
     // baseURL: 'http://localhost:4000',
-    baseURL: proccess.env.BASE_URL,
+    baseURL: proccess.env.REACT_APP_BASE_URL,
     withCredentials: true,
 })
 
