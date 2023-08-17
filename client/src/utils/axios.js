2f8f6da007ea7d5abe9574cd2216404a00375c5d
rahul4019 <rahulkashyap4019@gmail.com>
2023-08-17 18:08:49 +0530

    fix env error

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
     // baseURL: 'http://localhost:4000',
-    baseURL: proccess.env.REACT_APP_BASE_URL,
+    baseURL: process.env.REACT_APP_BASE_URL,
     withCredentials: true,
 })
 
