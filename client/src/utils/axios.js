ce5fe5213acdccdb4a1e127c56903ff0f8946233
rahul4019 <rahulkashyap4019@gmail.com>
2023-08-18 23:04:49 +0530

    Update api url

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
     // baseURL: 'http://localhost:4000',
-    baseURL: "https://airbnb-backend.uk.to",
+    baseURL: "https://airbnb-api.up.railway.app",
     withCredentials: true,
 })
 
