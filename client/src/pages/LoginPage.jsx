e5a07fe7699be4303875efa92c5a099cb2aab0bf
rahul4019 <rahulkashyap4019@gmail.com>
2023-09-16 23:08:25 +0530

    Fix user error in login page

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -14,7 +14,7 @@ const LoginPage = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [redirect, setRedirect] = useState(false);
-  const { login } = useContext(UserContext);
+  const { user, login } = useContext(UserContext);
 
   const handleFormSubmit = async (e) => {
     e.preventDefault();
