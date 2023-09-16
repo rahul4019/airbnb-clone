d262f020824dfbe1119ccd4419da69176dd01b17
rahul4019 <rahulkashyap4019@gmail.com>
2023-09-16 09:30:14 +0530

    Update google callback url in google passport strategy

--- api/config/passport.js
+++ api/config/passport.js
@@ -7,7 +7,7 @@ passport.use(
     new GoogleStrategy({
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: '/auth/google/callback'
+        callbackURL: 'https://airbnb-api.up.railway.app/auth/google/callback'
     },
         async function (accessToken, refreshToken, profile, done) {
             try {
