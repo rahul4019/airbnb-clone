2ce926cd11047eb78786ae7a6bd833b78b8c40e5
rahul4019 <rahulkashyap4019@gmail.com>
2023-09-18 18:09:01 +0530

    Update Profile page's UI

---
+++ client/src/components/ui/avatar.jsx
@@ -0,0 +1,33 @@
+import * as React from "react"
+import * as AvatarPrimitive from "@radix-ui/react-avatar"
+
+import { cn } from "@/lib/utils"
+
+const Avatar = React.forwardRef(({ className, ...props }, ref) => (
+  <AvatarPrimitive.Root
+    ref={ref}
+    className={cn("relative flex h-full w-full shrink-0 overflow-hidden rounded-full", className)}
+    {...props} />
+))
+Avatar.displayName = AvatarPrimitive.Root.displayName
+
+const AvatarImage = React.forwardRef(({ className, ...props }, ref) => (
+  <AvatarPrimitive.Image
+    ref={ref}
+    className={cn("aspect-square h-full w-full", className)}
+    {...props} />
+))
+AvatarImage.displayName = AvatarPrimitive.Image.displayName
+
+const AvatarFallback = React.forwardRef(({ className, ...props }, ref) => (
+  <AvatarPrimitive.Fallback
+    ref={ref}
+    className={cn(
+      "flex h-full w-full text-9xl items-center justify-center rounded-full bg-muted",
+      className
+    )}
+    {...props} />
+))
+AvatarFallback.displayName = AvatarPrimitive.Fallback.displayName
+
+export { Avatar, AvatarImage, AvatarFallback }
--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -3,9 +3,12 @@ import { toast } from 'react-toastify';
 import { Navigate, useParams } from 'react-router-dom';
 
 import AccountNav from '@/components/ui/AccountNav';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import { Button } from '@/components/ui/button';
 
 import PlacesPage from './PlacesPage';
 import { useAuth } from '../../hooks';
+import { LogOut, Mail, PenSquare, Text } from 'lucide-react';
 
 const ProfilePage = () => {
   const auth = useAuth();
@@ -39,15 +42,48 @@ const ProfilePage = () => {
     <div>
       <AccountNav />
       {subpage === 'profile' && (
-        <div className="mx-auto max-w-lg text-center">
-          Logged in as {user.name} ({user.email})
-          <br />
-          <button
-            className="mt-2 w-3/4 rounded-2xl bg-primary p-2 text-white md:w-full"
-            onClick={handleLogout}
-          >
-            Logout
-          </button>
+        <div className="m-4 flex flex-col items-center gap-8 rounded-[10px]  p-4 sm:h-1/5 sm:flex-row sm:items-stretch lg:gap-28 lg:pl-32 lg:pr-20">
+          {/* avatar */}
+          <div className="flex h-40 w-40 justify-center sm:h-72 sm:w-72 md:h-96 md:w-96">
+            <Avatar>
+              <AvatarImage src="https://cdn-icons-png.flaticon.com/512/4140/4140048.png" />
+              <AvatarFallback>{user.name.slice([0], [1])}</AvatarFallback>
+            </Avatar>
+          </div>
+
+          <div className="flex grow flex-col items-center gap-10 sm:items-start sm:justify-around sm:gap-0">
+            {/* user details */}
+            <div className="flex flex-col items-center gap-2 sm:items-start">
+              <div className="flex items-center gap-2">
+                <Text height="18" width="18" />
+                <div className="text-xl">
+                  <span>Name: </span>
+                  <span className="text-gray-600">{user.name}</span>
+                </div>
+              </div>
+              <div className="flex items-center gap-2">
+                <Mail height="18" width="18" />
+                <div className="text-xl">
+                  <span>Email: </span>
+                  <span className="text-gray-600">{user.email}</span>
+                </div>
+              </div>
+              <p></p>
+            </div>
+
+            {/* Action buttons */}
+            <div className="flex w-full justify-around sm:justify-end sm:gap-5 md:gap-10">
+              {/* <Button varient="secondary">Edit profile</Button> */}
+              <Button className="bg-blue-600 hover:bg-blue-600 ">
+                <PenSquare className="mr-2 h-4 w-4" />
+                Edit Profile
+              </Button>
+              <Button variant="secondary" onClick={handleLogout}>
+                <LogOut className="mr-2 h-4 w-4" />
+                Logout
+              </Button>
+            </div>
+          </div>
         </div>
       )}
       {subpage === 'places' && <PlacesPage />}
