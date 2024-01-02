c26a14a63787df6d5f38e4e1a74ab5eaa2eea4de
georgeyoumansjr <georgeyoumansjr@gmail.com>
2024-01-02 07:31:18 -0500

    remv classname issue

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -37,11 +37,11 @@ function App() {
               <Route index element={<IndexPage />} />
               <Route path="/login" element={<LoginPage />} />
               <Route path="/register" element={<RegisterPage />} />
+              <Route path="/place/:id" element={<PlacePage />} />
               <Route path="/account" element={<ProfilePage />} />
               <Route path="/account/places" element={<PlacesPage />} />
               <Route path="/account/places/new" element={<PlacesFormPage />} />
               <Route path="/account/places/:id" element={<PlacesFormPage />} />
-              <Route path="/place/:id" element={<PlacePage />} />
               <Route path="/account/bookings" element={<BookingsPage />} />
               <Route
                 path="/account/bookings/:id"
--- client/src/components/ui/PlaceCard.jsx
+++ client/src/components/ui/PlaceCard.jsx
@@ -26,7 +26,7 @@ const PlaceCard = ({ place }) => {
         <div className='mt-1 flex justify-between'>
           <h2 className="truncate font-bold">{address}</h2>
           <div className='flex'>
-          <svg class="w-5 h-5 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+          <svg className="w-5 h-5 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
             <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
           </svg>
           <span className='ml-1 text-md font-semibold'> {avgRating.toFixed(2)}</span>
