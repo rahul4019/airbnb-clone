afb17c9682625d6e2df731bcdb970c136ce13ee6
georgeyoumansjr <georgeyoumansjr@gmail.com>
2024-01-05 10:00:01 -0500

    sendEmail utils +

---
+++ api/utils/sendEmail.js
@@ -0,0 +1,29 @@
+const nodemailer = require("nodemailer");
+
+const sendEmail = async (email, subject, text) => {
+    try {
+        const transporter = nodemailer.createTransport({
+            host: process.env.HOST,
+            service: process.env.SERVICE,
+            port: 587,
+            secure: true,
+            auth: {
+                user: process.env.USER,
+                pass: process.env.PASS,
+            },
+        });
+
+        await transporter.sendMail({
+            from: process.env.USER,
+            to: email,
+            subject: subject,
+            text: text,
+        });
+
+        console.log("email sent sucessfully");
+    } catch (error) {
+        console.log(error, "email not sent");
+    }
+};
+
+module.exports = sendEmail;
